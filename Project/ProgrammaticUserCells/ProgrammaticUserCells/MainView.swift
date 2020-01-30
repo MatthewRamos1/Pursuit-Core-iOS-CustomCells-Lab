0aab413b01b3bcadf91467163b0ff2084dea013d
Matthew Ramos <mmramos309@gmail.com>
2020-01-30 16:09:41 -0500

    Created xib and custom collection view cell within it, created main view file

---
+++ Project/ProgrammaticUserCells/ProgrammaticUserCells/MainView.swift
@@ -0,0 +1,35 @@
+//
+//  MainView.swift
+//  ProgrammaticUserCells
+//
+//  Created by Matthew Ramos on 1/30/20.
+//  Copyright © 2020 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    public lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 400, height: 400)
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundColor = .systemGreen
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        
+    }
+}
